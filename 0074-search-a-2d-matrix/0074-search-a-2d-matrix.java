c151174942d7833efea0544076c5ca020c6d951b
Nisuri Munasinghe <54243363+NisuriMunasinghe@users.noreply.github.com>
2026-04-21 18:46:08 +0530

    Time: 0 ms (100%), Space: 43.9 MB (46.19%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -2,22 +2,43 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n=matrix[0].length;
-        for (int i=0; i<m; i++){
-            if (matrix[i][0]<=target && target<=matrix[i][n-1]){
-                if (i>0){
-                    if (matrix[i-1][n-1]>target && target<matrix[i][0]){
-                    return false;
-                    }
-                }
-                for (int j=0; j<n; j++){
-                    if(matrix[i][j]==target){
-                        return true;
-                    }
-                }
+        int low_row=0;
+        int high_row=m-1;
+        int low_column=0;
+        int high_column=n-1;
+        while (high_row>low_row){
+            int midrow=low_row+(high_row-low_row+1)/2;
+            if(target>=matrix[midrow][0]){
+                low_row=midrow;
             }
-
+            else if (target<=matrix[midrow-1][n-1]){
+                high_row=midrow-1;
+            }
+            else{
+                return false;
+            }
+        }
+        while (high_column>low_column){
+            int mid_column=low_column+(high_column-low_column+1)/2;
+            if(target==matrix[low_row][mid_column]){
+                return true;
+            }
+            else if(target>matrix[low_row][mid_column]){
+                low_column=mid_column;
+            }
+            else if(target<matrix[low_row][mid_column]){
+                high_column=mid_column-1;
+            }
+            else{
+                return false;
+            }
+        }
+        if (matrix[low_row][low_column]==target){
+            return true;
         }
         return false;
         
     }
 }
+
+
