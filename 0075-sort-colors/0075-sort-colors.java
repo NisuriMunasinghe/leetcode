646a9f78ed969b37b9326cd8269bf4b1a5507b9a
Nisuri Munasinghe <54243363+NisuriMunasinghe@users.noreply.github.com>
2026-04-23 13:19:14 +0530

    Time: 0 ms (100%), Space: 43.7 MB (27.17%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,9 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int sortnum[]=new int[nums.length];
         int white_min=0;//1
         int white_count=0;//1
-        int temp;
         for (int i=0; i<nums.length;i++){
             if (nums[i]==2){
                 nums[i]=2;
@@ -16,26 +14,14 @@ class Solution {
                 white_count++;
             }
             else if(nums[i]==0){
-                if(white_count!=0){
                 if(nums[white_min+white_count]==2){
                     nums[i]=2;
                 }
                 nums[white_min+white_count]=1;
                 nums[white_min]=0;
                 white_min++;
-                }
-                else if(white_count==0){
-                    if(nums[white_min]==2){
-                    nums[i]=2;
-                }
-                nums[white_min]=0;
-                white_min++;
-                }
-            }
-
-        
+                
+            }       
     }
-    
-
 }
 }
